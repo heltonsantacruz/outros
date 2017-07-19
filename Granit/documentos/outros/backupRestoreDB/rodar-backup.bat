c8ded3a29e7609d7605847de6425636203a41237
heltonsantacruz <heltonsantacruz@gmail.com>
2017-07-18 23:51:31 -0300

    Delete rodar-backup.bat

--- Granit/documentos/outros/backupRestoreDB/rodar-backup.bat
+++
@@ -1,5 +0,0 @@
-set datestr=%date:~6,4%%date:~0,2%%date:~3,2%
-set timestr=%time:~0,2%%time:~3,2%
-set data=%datestr%-%timestr%
-mkdir c:\backupgranit
-"c:\Program Files\MySQL\MySQL Server 5.0\bin\mysqldump.exe" -ujpa -pjpa jpa > c:\backupgranit\backup%data%.sql
